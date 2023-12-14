74b8b346851ffd7cffba340f19d8b2a33426d3e0
dineshkerlepalli <krishnakerlepalli@gmail.com>
2023-12-14 09:02:38 +0530

    dvc init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Aims at recommending top3 food products in user home page',
+    author='Dinesh Kerlepalli',
+    license='',
+)
